cc053afbe460dadffd09724fb3a6b9e0d4ea652b
DiyaGhorpade <withdiya21@gmail.com>
2026-02-10 22:47:46 +0530

    Set up ai recommendation service-endpoints and db

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,28 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import jakarta.ws.rs.Path;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(()->new RuntimeException("No reco found"));
+    }
+}
